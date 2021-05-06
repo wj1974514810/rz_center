411698598594475c49107f002ec11f242bd41f0e
wj123 <1974514810@qq.com>
2021-05-06 17:12:00 +0800

    基本组织结构封装

---
+++ src/views/departments/components/tree-tools.vue
@@ -0,0 +1,49 @@
+  <!-- 插槽 -->
+<template>
+  <el-row type="flex" justify="space-between" align="middle" style="width:100%; height: 40px">
+    <el-col>
+      <span>{{ treeNod.name }}</span>
+    </el-col>
+    <el-col :span="4">
+      <el-row type="flex" justify="end">
+        <!-- 两个内容 -->
+        <el-col>{{ treeNod.manager }}</el-col>
+        <el-col>
+          <!-- 下拉菜单 element -->
+          <el-dropdown>
+            <span>
+              操作<i class="el-icon-arrow-down" />
+            </span>
+            <!-- 下拉菜单 -->
+            <el-dropdown-menu slot="dropdown">
+              <!--编辑部门和删除部门只会在子节点上显示-->
+              <el-dropdown-item v-if="!isRoot">添加部门</el-dropdown-item>
+              <el-dropdown-item>修改部门</el-dropdown-item>
+              <el-dropdown-item v-if="!isRoot">删除部门</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+        </el-col>
+      </el-row>
+    </el-col>
+  </el-row>
+</template>
+<script>
+export default {
+  props: {
+    treeNod: {
+      type: Object,
+      required: true
+    },
+    // 布尔值 控制父节点和子节点显示的部门数量
+    isRoot: {
+      type: Boolean,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
+
--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,19 +1,42 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        组织架构
-      </h2>
+      <el-card class="tree-card">
+        <Trees :tree-nod="componey" :is-root="true" />
+        <hr>
+        <el-tree :data="departs" :props="defaultProps">
+          <template #default="scoped">
+            <Trees :tree-nod="scoped.data" :is-root="false" />
+          </template>
+        </el-tree>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
+import Trees from './components/tree-tools'
 export default {
-
+  components: { Trees },
+  data() {
+    return {
+      componey: { name: '传智播客', manager: '负责人' },
+      defaultProps: {
+        label: 'name'
+      },
+      departs: [
+        { name: '总裁办', children: [{ name: '董事会' }] },
+        { name: '行政部' },
+        { name: '人事部' }
+      ]
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.tree-card {
+  padding: 30px  140px;
+  font-size:14px;
+}
 </style>
